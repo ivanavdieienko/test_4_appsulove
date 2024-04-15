482973538a821428e3ac5143a7fbc2bd82ab0902
Ivan Avdieienko <ivanavdieienko@gmail.com>
2024-04-15 10:55:21 +0200

    Colorize squares

--- Assets/Scripts/View/Square.cs
+++ Assets/Scripts/View/Square.cs
@@ -11,6 +11,14 @@ namespace AppsULove.TestGame
         [Inject]
         private SignalBus _signalBus;
 
+        private void Start()
+        {
+            var spriteRenderer = GetComponent<SpriteRenderer>();
+
+            // Generate a random color in HSV, then convert it to RGB
+            spriteRenderer.color = Color.HSVToRGB(UnityEngine.Random.value, 1f, 1f); ;
+        }
+
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.gameObject.CompareTag("Player"))
